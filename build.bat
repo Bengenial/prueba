df4281c7d8c5e9f0181187f011b692723cef29f8
Bengenial <you@example.com>
2025-06-23 20:30:54 -0400

    xd3

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ REM Crear carpeta build si no existe
 if not exist build mkdir build
 
 REM Compilar usando los headers y librerías locales
-gcc -Isrc/include -Wno-unused-result src/poker.c src/tdasPoker/*.c src/tdas/*.c -o build/poker.exe -Llib -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc src\poker.c src\tdasPoker\*.c src\tdas\*.c -Iinclude -o build\poker.exe -Llib -lraylib -lopengl32 -lgdi32 -lwinmm
 
 IF %ERRORLEVEL% NEQ 0 (
     echo ❌ Error en la compilación.
